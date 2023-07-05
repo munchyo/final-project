4b15da2457bd71ef67e65e0f6d8944b28da3435a
munchyo <m_chyo_@naver.com>
2023-07-05 16:03:31 +0900

    Authenticate

--- src/main/java/com/goodday/proj/aop/Authenticate.java
+++ src/main/java/com/goodday/proj/aop/Authenticate.java
@@ -1,22 +1,52 @@
 package com.goodday.proj.aop;
 
+import com.goodday.proj.constant.ErrorConst;
+import com.goodday.proj.exhandler.customex.LoginException;
+import com.goodday.proj.api.member.repository.MemberRepository;
+import com.goodday.proj.exhandler.customex.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Optional;
 
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class Authenticate {
 
-    @Before("execution(* com.goodday.proj.api.member..*(..))")
-    public void testAop() {
-        log.debug("come in member!!!!");
-        log.debug("come in member!!!!");
-        log.debug("come in member!!!!");
-        log.debug("come in member!!!!");
-        log.debug("come in member!!!!");
-        log.debug("come in member!!!!");
+    private final MemberRepository memberRepository;
+
+    @Before("execution(* com.goodday.proj.api.*.controller..*(..)) " +
+            "&& !execution(* com.goodday.proj.api.member..*(..)) " +
+            "&& !execution(* com.goodday.proj.api.mail..*(..)) " +
+            "&& !execution(* com.goodday.proj.api.calorie..*(..)) ")
+    public void loginAuthAOP() {
+        ServletRequestAttributes requestAttributes =
+                (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+
+        Optional<String> optionalMemberNo = Optional.ofNullable(request.getHeader("memberNo"));
+
+        if (optionalMemberNo.isEmpty()) throw new LoginException(ErrorConst.authError2);
+    }
+
+    @Before("execution(* com.goodday.proj.api.admin..*(..))")
+    public void adminAuthAOP() {
+        ServletRequestAttributes requestAttributes =
+                (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+
+        Optional<String> optionalMemberNo = Optional.ofNullable(request.getHeader("memberNo"));
+
+        if (memberRepository.findSessionMemberByNo(Long.valueOf(optionalMemberNo.get())).get().getAdmin().equals("N")) {
+            throw new NotFoundException();
+        }
     }
 }
